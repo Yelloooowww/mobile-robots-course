401bc82389eb84374c00d3cf30dbb887c3fa6497
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2020-11-13 15:15:30 +0800

    timer

---
+++ checkpoint3/catkin_ws/src/pkg/src/checkpoint3_timer.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import wiringpi
+import time
+import random
+import rospy
+from std_msgs.msg import Int32
+
+
+class Control(object):
+    def __init__(self):
+        wiringpi.wiringPiSetup()
+        wiringpi.pinMode(7, 0)
+        wiringpi.pinMode(8, 0)
+        wiringpi.pinMode(9, 0)
+
+        self.led=-1000
+        self.last_led=-1000
+        self.last_action=None
+        self.now_action=None
+        self.state="NoGoal"
+        self.time=0
+
+        self.sub_led = rospy.Subscriber("pub_led", Int32, self.cb_led, queue_size=1)
+        self.pub_int = rospy.Publisher("array", Int32, queue_size=1)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.control_loop)
+
+        print("init done")
+
+
+    def cb_led(self,msg):
+        self.led=msg.data
+
+
+    
+    def motor_control(self):
+        if self.now_action=="advance":
+            self.pub_int.publish(1)
+            print('~~~pub advance~~~')
+        if self.now_action=="right":
+            self.pub_int.publish(2)
+            print('~~~pub right~~~')
+        if self.now_action=="left":
+            self.pub_int.publish(3)
+            print('~~~pub left~~~')
+        if self.now_action=="back":
+            self.pub_int.publish(4)
+            print('~~~pub back~~~')
+        if self.now_action=="stop":
+            self.pub_int.publish(5)
+            print('~~~pub stop~~~')
+
+
+
+
+    def control_loop(self,event):
+        #for bump sensor
+        my_input7 = wiringpi.digitalRead(7)
+        my_input8 = wiringpi.digitalRead(8)
+        my_input9 = wiringpi.digitalRead(9)
+
+        if my_input7==0 and my_input8==0 and my_input9==1: #hit
+            self.now_action="back"
+            self.next_action="left"
+            self.state="Modify"
+            self.time=20
+            
+        if my_input7==1 and my_input8==0 and my_input9==1: #hit
+            self.now_action="back"
+            self.next_action="left"
+            self.state="Modify"
+            self.time=20
+
+        if my_input7==0 and my_input8==1 and my_input9==1: #hit
+            self.now_action="back"
+            self.next_action="right"
+            self.state="Modify"
+            self.time=20
+
+        if my_input7==1 and my_input8==1 and my_input9==1: #free
+            #for LED sensor
+            if self.led<150 and self.led>=100:
+                if self.state!="NearGoal":
+                    self.time=30
+                    self.now_action="left"
+                    self.next_action="right"
+                self.state=="NearGoal"    
+
+            if self.led<100:
+                self.state=="FindGoal"
+
+            if self.led>=150:
+                self.state=="NoGoal"
+
+        if my_input9==0 : #have catched the ball
+            self.state="Finish"
+
+
+
+
+        #update
+        if self.state=="Modify":
+            if self.time==10:
+                self.now_action=self.next_action
+                self.next_action=None
+            
+
+        if self.state=="NearGoal":
+            if self.time==20:
+                self.now_action=self.next_action
+                self.next_action=None
+        
+        if self.time<0:
+            self.state="NoGoal"
+            self.time=0
+
+
+        if self.state=="FindGoal":
+                self.now_action="advance"
+
+        if self.state=="NoGoal":
+            self.now_action="advance"
+
+        if self.state=="Finish":
+            self.now_action="stop"
+
+
+
+        
+        self.time=self.time-1
+        self.last_led=self.led
+        self.last_action=self.now_action
+        self.motor_control()    
+        print(self.led,my_input7,my_input8,my_input9,self.state,self.now_action,self.time)
+        
+
+
+
+if __name__ == "__main__":
+    rospy.init_node("Control")
+    control=Control()
+    rospy.spin()
